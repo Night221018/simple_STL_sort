0fedd9ca023ad33d9ef0336d6d96c66a076cbf23
luzelin <luzelin1024@163.com>
2023-02-01 15:38:02 +0000

    heap_v1

---
+++ head_sort.h
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: heap_sort.h
+	> Author: Luzelin 
+ ************************************************************************/
+
+#ifndef _HEAP_SORT_H
+#define _HEAP_SORT_H
+
+void heap_sort(RandomIter arr, int n, function<bool(int &, int &)> &point);
+
+#endif
+
