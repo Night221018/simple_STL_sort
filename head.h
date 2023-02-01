fc6a7d6f31dc6acfbb6c4af4deb5aac42ad98677
luzelin <luzelin1024@163.com>
2023-02-01 15:35:25 +0000

    v1

---
+++ head.h
@@ -0,0 +1,31 @@
+/*************************************************************************
+	> File Name: head.h
+	> Author: Luzelin 
+ ************************************************************************/
+
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <iostream>
+#include <cstdio>
+#include <functional>
+#include <ctime>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::function;
+using std::swap;
+
+#define threshold 16 
+
+#include "compare.h"
+#include "RandomIter.h"
+#include "unguarded_insert_sort.h"
+#include "unguarded_quick_sort.h"
+#include "heap_sort.h"
+#include "sort.h"
+#include "vector.h"
+
+#endif
+
